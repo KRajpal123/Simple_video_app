d2f70f79fd2586429559556bdfc383712037e6d9
Rajpal <rajpalreddyk@.com>
2023-07-21 20:18:51 +0530

    videos added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,23 +1,55 @@
-import { Stack,Box,Typography,Card,CardContent,CardMedia } from '@mui/material'
-import { CheckCircle } from '@mui/icons-material'
-import React from 'react'
-import { demoThumbnailUrl,demoChannelTitle,demoVideoUrl,demoProfilePicture,demoVideoTitle } from '../Utils/constants'
-import { Link } from 'react-router-dom'
-const Videos = ({videos}) => {
-  console.log(videos)
-  return (
-    <Stack direction={'row'} flexWrap={'wrap'} justifyContent={'start'} gap={2}>
-      {
-        videos.map((val,i) => {
-          return (
-            <Box key={i}>
-              <Typography sx={{color:"white"}}>{val.title}</Typography>
-            </Box>
-          )
-        })
-      }
-    </Stack>
-  )
-}
 
-export default Videos
+import { Stack } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import React from "react";
+import {
+  demoThumbnailUrl,
+  demoChannelTitle,
+  demoVideoUrl,
+  demoProfilePicture,
+  demoVideoTitle,
+} from "../Utils/constants";
+import { Link } from "react-router-dom";
+const Videos = ({ videos }) => {
+  console.log(videos);
+  return (
+    <Stack direction={"row"} flexWrap={"wrap"} justifyContent={"start"} gap={2}>
+      {videos.map((val, i) => {
+        return (
+          <Card sx={{ maxWidth: 345 }} key={i}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image={val.thumbnailUrl}
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+               {val.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+           <Link to={val.videoUrl}>
+           <Button size="small" color="primary">
+             Go
+            </Button>
+           </Link>
+          </CardActions>
+        </Card>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default Videos;
